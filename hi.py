c5eca26df05dc03ba0a8b6694366b7f3d877152d
dudeishan <styxnerthegamer@gmail.com>
2024-03-01 23:02:34 +1100

    json dumped
    json dumped from api

--- hi.py
+++ hi.py
@@ -1,18 +1,21 @@
 import ikea_api
-
+import json
+# impr
 # Constants like country, language, base url
 constants = ikea_api.Constants(country="au", language="en")
 # Search API
 search = ikea_api.Search(constants)
 # Search endpoint with prepared data
-endpoint = search.search("Billy")
+endpoint = search.search("Table")
 
 
 json_dump =ikea_api.run(endpoint)
 
-i= 0
-for key in json_dump:
-    if i ==0:
-        print(key, json_dump[key])
-        i+=1
-
+# i= 0
+# for key in json_dump:
+#     print(key)
+json_str = json.dumps(json_dump, indent=4)
+print(json_str)
+with open('dump.json','w+') as f:
+    f.write(json_str)
+    # print()
