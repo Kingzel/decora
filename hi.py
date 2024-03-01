937ab2268227e79269cbeac363bdf9c822dd5cb7
dudeishan <styxnerthegamer@gmail.com>
2024-03-01 22:42:55 +1100

    api test

--- hi.py
+++ hi.py
@@ -1,1 +1,18 @@
-print('fahad >:)')
+import ikea_api
+
+# Constants like country, language, base url
+constants = ikea_api.Constants(country="au", language="en")
+# Search API
+search = ikea_api.Search(constants)
+# Search endpoint with prepared data
+endpoint = search.search("Billy")
+
+
+json_dump =ikea_api.run(endpoint)
+
+i= 0
+for key in json_dump:
+    if i ==0:
+        print(key, json_dump[key])
+        i+=1
+
