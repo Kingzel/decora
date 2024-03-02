173bf7e52f4cbe99aa3b266b538f64806e961f2e
dudeishan <styxnerthegamer@gmail.com>
2024-03-02 20:16:10 +1100

    fixed

--- decora.py
+++ decora.py
@@ -1,11 +1,10 @@
 from PIL import Image
 from transformers import pipeline
 import os
-from enum import Enum
 
 
 
-questions=[   "describe vibe",
+questions=["describe vibe",
     "color", "type of design furniture", 'identify furniture']
     
 # b
@@ -13,7 +12,6 @@ questions=[   "describe vibe",
 # question = "vibe of this object"
 # question2 = 'is this danger'
 # question3 = 'color
-enumerate
 
 def query(url: str, question: str, k : int):
     vqa_pipeline = pipeline("visual-question-answering", model="dandelin/vilt-b32-finetuned-vqa")
