c8ae0959542d8b882785bead3af51adf4c1ad546
dudeishan <styxnerthegamer@gmail.com>
2024-03-04 00:04:53 +1100

    Squashed commit of the following:
    commit edb229ac97e7f207c66a96ae5916ed7ab461ee39
    Author: dudeishan <styxnerthegamer@gmail.com>
    Date:   Sun Mar 3 17:11:48 2024 +1100
    
        show
    
    commit 8a9b6b10eaf24eea0f090b750deced688f84ad6c
    Author: dudeishan <styxnerthegamer@gmail.com>
    Date:   Sun Mar 3 16:44:34 2024 +1100
    
        done
    
    commit b8c07c8d3ac022dfdc1a944e9a84a23baa5a9564
    Author: dudeishan <styxnerthegamer@gmail.com>
    Date:   Sun Mar 3 16:20:35 2024 +1100
    
        done with project
    
    commit d7c9acbce76de3aa4b90693854eb54ac2e0b1907
    Merge: 4329fe0 9334e89
    Author: dudeishan <styxnerthegamer@gmail.com>
    Date:   Sun Mar 3 16:12:39 2024 +1100
    
        Merge branch 'finished-proj' of https://github.com/Kingzel/decora into finished-proj
    
    commit 4329fe09f65dc098aa085b5f604726478b959093
    Author: dudeishan <styxnerthegamer@gmail.com>
    Date:   Sun Mar 3 16:12:29 2024 +1100
    
        html done
    
    commit 9334e89a081ba77e98c44f83a62f732ef3fea662
    Author: iffatazeez <126850039+iffatazeez@users.noreply.github.com>
    Date:   Sun Mar 3 16:11:14 2024 +1100
    
        modularised
    
    commit 5d4c92506a03078aee1793cbd9fe99580d2d77e4
    Author: Tavishi <tsax0001@student.monash.edu>
    Date:   Sun Mar 3 15:39:07 2024 +1100
    
        lorem ipsum removed
    
    commit 37c04db3e511900aa29874844ff12c8cc88b2522
    Author: dudeishan <styxnerthegamer@gmail.com>
    Date:   Sun Mar 3 15:32:50 2024 +1100
    
        Squashed commit of the following:
    
        commit 96b7123b4861f6db007c0ca4cd8d326a0e610e1d
        Author: Tavishi <tsax0001@student.monash.edu>
        Date:   Sun Mar 3 15:29:47 2024 +1100
    
            frontend
    
        commit 112d2664941dfde7814b6ab8d0bb8f75903b7e0c
        Author: Tavishi <tsax0001@student.monash.edu>
        Date:   Sun Mar 3 13:06:12 2024 +1100
    
            frontend completed
    
        commit 8d3015052a8f85e277d473263215a9bd0af7de85
        Author: Tavishi <tsax0001@student.monash.edu>
        Date:   Sun Mar 3 12:45:06 2024 +1100
    
            tavtest
    
    commit 427f3a1850825e9f633adf9c3092c08e5cf55d65
    Author: dudeishan <styxnerthegamer@gmail.com>
    Date:   Sun Mar 3 15:31:39 2024 +1100
    
        deleted web stuff
    
    commit 793703efe52e23826fc9feafd9cc20019b409426
    Author: dudeishan <styxnerthegamer@gmail.com>
    Date:   Sun Mar 3 15:30:48 2024 +1100
    
        renamed sender file
    
    commit e13e73da9a6c04e29e162ff0789f67cea52a8716
    Author: dudeishan <styxnerthegamer@gmail.com>
    Date:   Sun Mar 3 15:21:37 2024 +1100
    
        done

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+from sender import gen_everything
 
 app = Flask(__name__, template_folder='web', static_folder='web/stat')
+app.secret_key = 'super secret key'
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -15,10 +18,19 @@ def search():
 
 @app.route("/process_url", methods=['POST'])
 def process_url():
-    image_url = request.form['image_url']
-    print(image_url)
-    return redirect(url_for('home'))  # Example redirect
+    if request.method == 'POST':
+        session['url'] = request.form['url']
+        session['art'] = request.form['art']
+        print(session['url'],session['art'])
 
+
+    return redirect(url_for('result'))  # Example redirect
+
+@app.route("/result")
+def result():
+    dump = gen_everything(session['url'],session['art'])
+    print(dump)
+    return render_template("test_results.html", my_list =dump)
 
 if __name__ == "__main__":
     app.run(debug=True)
