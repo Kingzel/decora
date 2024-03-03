96b7123b4861f6db007c0ca4cd8d326a0e610e1d
Tavishi <tsax0001@student.monash.edu>
2024-03-03 15:29:47 +1100

    frontend

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__, template_folder='web', static_folder='web/stat')
 @app.route("/")
@@ -12,6 +12,13 @@ def aboutus():
 @app.route("/search")
 def search():
     return render_template("projects.html")
+
+@app.route("/process_url", methods=['POST'])
+def process_url():
+    image_url = request.form['image_url']
+    print(image_url)
+    return redirect(url_for('home'))  # Example redirect
+
 
 if __name__ == "__main__":
     app.run(debug=True)
