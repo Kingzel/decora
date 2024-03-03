4329fe09f65dc098aa085b5f604726478b959093
dudeishan <styxnerthegamer@gmail.com>
2024-03-03 16:12:29 +1100

    html done

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__, template_folder='web', static_folder='web/stat')
+app.secret_key = 'super secret key'
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -15,10 +17,16 @@ def search():
 
 @app.route("/process_url", methods=['POST'])
 def process_url():
-    image_url = request.form['image_url']
-    print(image_url)
-    return redirect(url_for('home'))  # Example redirect
+    if request.method == 'POST':
+        session['url'] = request.form['url']
+        print(session['url'])
 
+
+    return redirect(url_for('result'))  # Example redirect
+
+@app.route("/result")
+def result():
+    return render_template("test_results.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
